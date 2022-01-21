6d2b04448c25473a5468d1950a8d1616ee186912
Ines <ines.perria@gmail.com>
2022-01-21 08:36:56 +0100

    Test sans pousser reellement le react

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
 import React from 'react';
 
-export default () => <div className="App">React Course 1</div>;
+// export default () => <div className="App">React Course 1</div>;
+
+export default () => <div className="App"> <h1> React Course 1 </h1></div>;
